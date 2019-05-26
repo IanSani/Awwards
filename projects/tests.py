910a745ad0bb853cba04637388ef9898309b3808
IanSani <iansani259@gmail.com>
2019-05-26 22:45:56 +0300

    Created project test class

--- projects/tests.py
+++ projects/tests.py
@@ -18,3 +18,28 @@ class ProfileTest(TestCase):
         self.profile.save()
         profile = Profile.get_profile()
         self.assertTrue(len(profile) > 0)
+
+class ProjectTest(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create(id = 1, username='zyzu')
+        self.profile = Profile.objects.create(user = self.user,bio = 'blow away',phone= 2356789)
+
+        self.project = Project.objects.create(posted_by = self.user,
+                                          profile = self.profile,
+                                          title = 'Blog',
+                                          description='turn up',
+                                          project_link= 'https://twingit.herokuapp.com/')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.project,Project))
+
+    def test_get_projects(self):
+        self.project.save()
+        project = Project.get_projects()
+        self.assertTrue(len(project) == 1)
+
+    def test_find_project(self):
+        self.project.save()
+        project = Project.find_project('blog')
+        self.assertTrue(len(project) > 0)
