0d1542e43465badbee6960eb6c3c447c1ea3d404
IanSani <iansani259@gmail.com>
2019-05-26 22:22:46 +0300

    Created project class

--- projects/models.py
+++ projects/models.py
@@ -25,3 +25,59 @@ class Profile(models.Model):
         profile = Profile.objects.all()
 
         return profile
+
+class Project(models.Model):
+    posted_by = models.ForeignKey(User, null=True)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE,null=True)
+    title = models.CharField(max_length=100, null=True)
+    project_image = models.ImageField(upload_to='projects/',null=True)
+    description = models.TextField(null=True)
+    project_link = models.TextField(null=True)
+
+    @classmethod
+    def get_projects(cls):
+        projects = Project.objects.all()
+        return projects
+
+    @classmethod
+    def find_project(cls,search_term):
+        project = Project.objects.filter(title__icontains=search_term)
+        return project
+
+    def design_rating(self):
+        all_designs =list( map(lambda x: x.design, self.reviews.all()))
+        return np.mean(all_designs)
+
+    def usability_rating(self):
+        all_usability =list( map(lambda x: x.usability, self.reviews.all()))
+        return np.mean(all_usability)
+
+    def content_rating(self):
+        all_content =list( map(lambda x: x.content, self.reviews.all()))
+        return np.mean(all_content)
+
+
+class Reviews(models.Model):
+    RATING_CHOICES = (
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+        (5, '5'),
+        (6, '6'),
+        (7, '7'),
+        (8, '8'),
+        (9, '9'),
+        (10, '10'),
+    )
+    juror = models.ForeignKey(User, on_delete=models.CASCADE,null=True)
+    project = models.ForeignKey(Project,on_delete=models.CASCADE, related_name='reviews',null=True)
+    design = models.IntegerField(choices=RATING_CHOICES,default=0)
+    usability = models.IntegerField(choices=RATING_CHOICES,default=0)
+    content = models.IntegerField(choices=RATING_CHOICES,default=0)
+    comment = models.CharField(max_length=200,null=True)
+
+    @classmethod
+    def get_reviews(cls):
+        reviews = Reviews.objects.all()
+        return reviews
